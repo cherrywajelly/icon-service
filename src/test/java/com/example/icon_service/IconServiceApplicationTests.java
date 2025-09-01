93e042d92330917b037c7f0b19e16d2746efc632
munyeong0103 <skymy02@ajou.ac.kr>
2025-09-01 18:24:41 +0900

    :tada: begin new service

---
+++ src/test/java/com/example/icon_service/IconServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icon_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
